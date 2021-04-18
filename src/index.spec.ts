bd6da872c7c6fbfbd0d4c82e68163a7f5bceec69
Satoshi Onoda <satoshionoda@gmail.com>
2021-04-18 18:21:33 +0900

    initial commit

---
+++ src/index.spec.ts
@@ -0,0 +1,41 @@
+import { __TEST__ } from "./index";
+
+describe("findSeries", () => {
+  const findSeries = __TEST__.findSeries;
+  test("it should work", () => {
+    const result = findSeries("第２節第１日");
+    expect(result).toBe(2);
+  });
+  test("it should return null when not found", () => {
+    const result = findSeries("第第１日");
+    expect(result).toBe(null);
+  });
+});
+
+describe("separateScore", () => {
+  const separateScore = __TEST__.separateScore;
+  test("it should work", () => {
+    const result = separateScore("13-1");
+    expect(result[0]).toBe(13);
+    expect(result[1]).toBe(1);
+  });
+  test("it should return null when not found", () => {
+    const result = separateScore("未定");
+    expect(result).toBe(null);
+  });
+});
+
+describe("parseDate", () => {
+  const parseDate = __TEST__.parseDate;
+  test("it should work", () => {
+    const result1 = parseDate("02/27(土)");
+    expect(result1).toBe("02/27");
+
+    const result2 = parseDate("05/04(火・祝)");
+    expect(result2).toBe("05/04");
+  });
+  test("it should return empty string when not found", () => {
+    const result = parseDate("");
+    expect(result).toBe("");
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,102 @@
+import fetch from "cross-fetch";
+
+const fs = require("fs-extra");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+//
+const url: [string, number, number][] = [
+  ["j1", 2021, 492],
+  ["j1", 2020, 477],
+  ["j1", 2019, 460],
+];
+
+const getData = async (category: string, year: number, id: number) => {
+  try {
+    const res = await fetch(
+      `https://data.j-league.or.jp/SFMS01/search?competition_years=${year}&competition_frame_ids=1&competition_ids=${id}&tv_relay_station_name=`
+    );
+    const html = await res.text();
+    const dom = new JSDOM(html);
+    const matches: MatchData[] = parseMatches(dom.window.document);
+    const dir = `${process.cwd()}/dist/`;
+    await fs.ensureDir(dir);
+    await fs.writeJSON(`${dir}/matches-${category}-${year}.json`, matches);
+    sleep(1000);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const parseMatches = (doc: HTMLDocument): MatchData[] => {
+  const result: MatchData[] = [];
+  doc.querySelectorAll(".search-table tbody tr").forEach((elm, i) => {
+    const seriesCol = elm.querySelector("td:nth-child(3)");
+    const series: number = findSeries(seriesCol.textContent);
+    const homeCol = elm.querySelector("td:nth-child(6) a");
+    const homeTeam = homeCol.textContent;
+    const awayCol = elm.querySelector("td:nth-child(8) a");
+    const awayTeam = awayCol.textContent;
+    const scoreCol = elm.querySelector("td:nth-child(7)");
+    const score: [number, number] = separateScore(scoreCol.textContent);
+    const [homeScore, awayScore] = score ? score : [null, null];
+    const yearCol = elm.querySelector("td:nth-child(1)");
+    const dateCol = elm.querySelector("td:nth-child(4)");
+    const date = `${yearCol.textContent}/${parseDate(dateCol.textContent)}`;
+
+    result.push({
+      series,
+      homeTeam,
+      awayTeam,
+      homeScore,
+      awayScore,
+      date,
+    });
+  });
+  return result.sort((a, b) => a.series - b.series);
+};
+
+const parseDate = (str: string): string => {
+  const found = /(\d+\/\d+)/.exec(str);
+  return found ? found[1] : "";
+};
+
+const separateScore = (str: string): [number, number] => {
+  const found = /(.*)-(.*)/.exec(str);
+  return found ? [parseInt(found[1]), parseInt(found[2])] : null;
+};
+
+const findSeries = (str: string): number => {
+  const half = toHalfWidth(str);
+  const found = /第(.*)節/.exec(half);
+  return found ? parseInt(found[1]) : null;
+};
+
+const toHalfWidth = (input: string): string => {
+  return input.replace(/[！-～]/g, function (input) {
+    return String.fromCharCode(input.charCodeAt(0) - 0xfee0);
+  });
+};
+
+const sleep = (ms: number): Promise<null> => {
+  return new Promise<null>((resolve) => setTimeout(resolve, ms));
+};
+
+url.forEach(([category, year, id]) => {
+  getData(category, year, id);
+});
+
+export const __TEST__ = { findSeries, separateScore, parseDate };
+
+export type MatchData = {
+  series: number;
+  homeTeam: string;
+  awayTeam: string;
+  homeScore: number;
+  awayScore: number;
+  date: string;
+};
+
+export type MatchPoint = {
+  team: string;
+  pt: number[];
+};
